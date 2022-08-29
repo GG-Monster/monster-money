12746ab4524392f87c1461b1d08dada4e36c9210
GG-Monster <lilili925@icloud.com>
2022-08-29 23:11:33 +0800

    deploy

---
+++ precache-manifest.28763dd61ec4693d590c44650d1fc6b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb5c4920b228571e606",
+    "url": "/monster-money/css/app.82344ff3.css"
+  },
+  {
+    "revision": "523589325b3b74bfeeffb377565bcabe",
+    "url": "/monster-money/index.html"
+  },
+  {
+    "revision": "cdb5c4920b228571e606",
+    "url": "/monster-money/js/app.1d23555d.js"
+  },
+  {
+    "revision": "c8227d483ccb89111456",
+    "url": "/monster-money/js/chunk-vendors.8a055649.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/monster-money/manifest.json"
+  },
+  {
+    "revision": "94e1bd7439ff00147a7a83d5f3e9380b",
+    "url": "/monster-money/qr_code.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/monster-money/robots.txt"
+  }
+]);
